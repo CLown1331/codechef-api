998720a60d37cbcbbb74fc7e6fe142bf7378e511
CLown1331 <clown1331@gmail.com>
2020-03-27 15:35:05 +0600

    rating

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 "use strict"
 const express = require("express");
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const _ = require('lodash');
+
 const port = process.env.PORT || 8080
 
 const app = express();
@@ -7,6 +11,39 @@ const app = express();
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
+
+app.get('/rating/:userName', async (req, res) => {
+    console.log(`fetching`);
+    const userName = req.params.userName;
+    if (!userName) {
+        res.send();
+    }
+    const result = await getUserInfo(userName);
+    console.log(result);
+    res.send(result);
+});
+
+async function getUserInfo (username) {
+    try {
+        console.log(`fetching rating for ${username}`);
+        const response = await fetch(`https://www.codechef.com/users/${username}`);
+        const userPage = await response.text();
+        const $ = cheerio.load(userPage);
+        const rating = $('.rating-number').first().text() || null;
+        const lastParticipation = $('.time').first().text() || null;
+        console.log(rating, !!rating);
+        console.log(lastParticipation, !!lastParticipation);
+        console.log('---\n');
+        return {
+            username,
+            rating,
+            lastParticipation,
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
