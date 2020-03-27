406d19a56adf15cd7ecaedc4a76b6b1b919fc910
CLown1331 <clown1331@gmail.com>
2020-03-27 15:00:26 +0600

    health check

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict"
+const express = require("express");
+const port = process.env.PORT || 8080
+
+const app = express();
+
+app.get('ping', (req, res) => {
+    res.send('pong');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
